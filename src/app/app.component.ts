63ea50233628c97cd0aba5c4bff4d7e638a8d04e
Vishesh Kumar yadav <107811025+visheshkumaryadav@users.noreply.github.com>
2023-10-28 13:46:12 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'demo';
+
+  submit(form:any){
+    var firstName = form.firstName;
+    console.log(firstName);
+  
+    var lastName = form.lastName;
+    console.log(lastName);
+  
+    var comment = form.comment;
+    console.log(comment);
+  }
+}
